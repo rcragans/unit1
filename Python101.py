c2d2b8377de930c03251d316165081c75fdf8307
rcragans <rcragans@gmail.com>
2018-11-13 14:28:22 -0500

    Add operators

--- Python101.py
+++ Python101.py
@@ -1,31 +1,37 @@
-print ("Hello, World")
-print ("""
-   It was a dark and stormy night.
-   A murder happened.
-""")
+# print ("Hello, World")
+# print ("""
+#    It was a dark and stormy night.
+#    A murder happened.
+# """)
 
-theBestClass = ("the 11/18 immersive")
-print (theBestClass)
+# theBestClass = ("the 11/18 immersive")
+# print (theBestClass)
 
-month = "November"
-print (type(month))
+# month = "November"
+# print (type(month))
 
-date = 13
-print type(date)
+# date = 13
+# print type(date)
 
-dateAsFloat = 13.0
-print type(dateAsFloat)
+# dateAsFloat = 13.0
+# print type(dateAsFloat)
 
-abool = True
-print type(abool)
+# abool = True
+# print type(abool)
 
-aList = []
-print type(aList)
+# aList = []
+# print type(aList)
 
-aDictionary = {}
-print type(aDictionary)
+# aDictionary = {}
+# print type(aDictionary)
 
+# first = "Robert"
+# last = "Ragans"
+# fullName = first + ' ' + last
+# print fullName
 
+# fourteen = 10 + 4 
+# print fourteen
 
 
 
