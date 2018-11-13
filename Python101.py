17a656ecb592a90d5ffaeb00079a66ccaf5851ab
rcragans <rcragans@gmail.com>
2018-11-13 14:03:27 -0500

    data types

--- Python101.py
+++ Python101.py
@@ -7,5 +7,25 @@ print ("""
 theBestClass = ("the 11/18 immersive")
 print (theBestClass)
 
+month = "November"
+print (type(month))
+
+date = 13
+print type(date)
+
+dateAsFloat = 13.0
+print type(dateAsFloat)
+
+abool = True
+print type(abool)
+
+aList = []
+print type(aList)
+
+aDictionary = {}
+print type(aDictionary)
+
+
+
 
 
