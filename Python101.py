b18c9742f9f575c6f1bf8172bff740db0861d23e
rcragans <rcragans@gmail.com>
2018-11-14 13:25:21 -0500

    Game Commit

--- Python101.py
+++ Python101.py
@@ -1,37 +1,37 @@
-# print ("Hello, World")
-# print ("""
-#    It was a dark and stormy night.
-#    A murder happened.
-# """)
+import random
+userName = raw_input("What is your name? ")
+secret_number = random.randint(1,10)
+gameOn = True
+allowedGuesses = 5
+userGuesses = 0
+keepPlaying = True
 
-# theBestClass = ("the 11/18 immersive")
-# print (theBestClass)
+while(keepPlaying):
 
-# month = "November"
-# print (type(month))
-
-# date = 13
-# print type(date)
-
-# dateAsFloat = 13.0
-# print type(dateAsFloat)
-
-# abool = True
-# print type(abool)
-
-# aList = []
-# print type(aList)
-
-# aDictionary = {}
-# print type(aDictionary)
-
-# first = "Robert"
-# last = "Ragans"
-# fullName = first + ' ' + last
-# print fullName
-
-# fourteen = 10 + 4 
-# print fourteen
-
-
-
+    while(gameOn):
+        userGuess = input("Guess a number between 1 and 10: ")
+        userGuesses += 1
+        if (int(userGuess) == secret_number):
+            gameOn = False
+            print ("Great job %s. Game Over" % userName)
+        else:
+            if (userGuesses == allowedGuesses):
+                gameOn = False
+                print ("You are out of guesses! The number was %i" % secret_number)
+            elif (int(userGuess) > int(secret_number)):
+                print ("%s, %i is too high" % (userName,userGuess))
+                print ("You have %i guesses left!") % (int(allowedGuesses)-int(userGuesses))
+            else:
+                print ("%s, %i is too low" % (userName, userGuess))
+                print ("Guess again...")  
+                print ("You have %i guesses left!") % (int(allowedGuesses)-int(userGuesses)) 
+    playAgain = raw_input("Would you like to play again? Y or N")
+    if (playAgain == "N"):
+        keepPlaying = False
+        print ("Thanks for playing, %s" % userName)
+    elif(playAgain == "Y"):
+        secret_number = random.randint(1,10)
+        userGuesses = 0
+        gameOn = True
+    else:
+        print ("Huh?")    
